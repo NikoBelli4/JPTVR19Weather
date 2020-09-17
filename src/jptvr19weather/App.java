de0a1f6d750c27a8a4f71b41bb88f3414e91091e
pupil <pupil@ivkhk.ee>
2020-09-17 10:48:54 +0300

    массив с числами, кототрые зависят от температуры

--- src/jptvr19weather/App.java
+++ src/jptvr19weather/App.java
@@ -9,10 +9,38 @@ import java.util.Random;
 
 /**
  *
- * @author Melnikov
+ * @author Niko_Belli4
  */
 class App {
     public void run(){
+        System.out.println("------WEATHER-------");
+        int min = -30;
+        int max = 35;
+        int n = 0;
+        Random random = new Random();
+        int [][] tempInYear = new int[12][];
+        for (int i = 0; i < 12; i++) {
+            switch (i) {
+                case 0: min=-20; max = 0; n=31; break;
+                case 1: min=-20; max = 5; n=28; break;
+                case 2: min=-15; max = 15; n=31; break;
+                case 3: min=-5; max = 20; n=01; break;
+                case 4: min=0; max = 25; n=31; break;
+                case 5: min=5; max = 30; n=30; break;
+                case 6: min=10; max = 30; n=31; break;
+                case 7: min=10; max = 25; n=31; break;
+                case 8: min=0; max = 20; n=30; break;
+                case 9: min=-5; max = 10; n=31; break;
+                case 10: min=-10; max = 5; n=30; break;
+                case 11: min=-20; max = 5; n=31; break;
+            }
+            tempInYear[i] = new int[n];
+            for (int j = 0; j < tempInYear[i].length; j++) {
+                tempInYear[i][j]=random.nextInt(max-min+1)+min;
+                System.out.printf("%4d",tempInYear[i][j]);
+            }
+            System.out.println();
+        }
         
     }
 }
